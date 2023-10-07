bfc85ac9503b7929fa31caa1bb03ae8ce140f661
Sooraj Ep <epsooraj4@gmail.com>
2023-10-07 22:25:05 +0530

    Cleaning

--- model_generator/admin.py
+++ model_generator/admin.py
@@ -1,15 +1,1 @@
 from django.contrib import admin
-
-from . import models
-
-
-class BaseModelAdmin(admin.TabularInline):
-    model = models.BaseModelGenerator
-    pass
-
-class BaseAppsAdmin(admin.ModelAdmin):
-    '''
-    '''
-    inlines = (BaseModelAdmin,)
-
-admin.site.register(models.BaseApps, BaseAppsAdmin)
