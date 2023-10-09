8719781b9449ad8288d0d034faa0daa5b42b328c
Sooraj Ep <epsooraj4@gmail.com>
2023-10-10 01:12:43 +0530

    Registering nested admin

--- model_generator/admin.py
+++ model_generator/admin.py
@@ -1,1 +1,25 @@
 from django.contrib import admin
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin, NestedTabularInline
+
+from . import models
+
+
+class ModelFieldParameterAdmin(NestedStackedInline):
+    model = models.FieldParameter
+
+
+class ModelFieldAdmin(NestedTabularInline):
+    model = models.ModelField
+    inlines = [ModelFieldParameterAdmin,]
+    extra = 1
+
+
+@admin.register(models.BaseModel)
+class BaseModelAdmin(NestedModelAdmin):
+    model = models.BaseModel
+    inlines = [ModelFieldAdmin,]
+
+
+@admin.register(models.BaseApp)
+class BaseAppAdmin(admin.ModelAdmin):
+    list_display = ('name',)
